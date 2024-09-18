184fa9a42e5b27f5fe48c8f4acc205ebefe87699
Ansh Bansal <anshofficial5255@gmail.com>
2024-09-18 21:50:15 +0530

    Views of delete_image done

--- backend/attendify/deleteImage/urls.py
+++ backend/attendify/deleteImage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('delete_image/', views.delete_image, name='delete_image'),
+    path('delete_image/<str:public_id>/', views.delete_image, name='delete_image'),
 ]

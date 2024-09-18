11275f6eb517d48fcdff08c1c50705d22f455613
rahul_004 <rahulbky2004@gmail.com>
2024-09-18 18:08:16 +0530

    Testing API

--- backend/attendify/getAttendanceList/urls.py
+++ backend/attendify/getAttendanceList/urls.py
@@ -4,3 +4,5 @@ from . import views
 urlpatterns = [
     path('fetch_students/', views.fetch_students, name='fetch_students'),
 ]
+
+# http://localhost:8000/getAttendanceList/fetch_students/?year=2022&batch=4&branch=CSE

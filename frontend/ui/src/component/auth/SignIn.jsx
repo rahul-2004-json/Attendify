779d238af0fe81daa6242da0d809795ffc56d3dd
Ansh Bansal <anshofficial5255@gmail.com>
2024-09-19 19:04:06 +0530

    Sign in page ui completed except images

--- frontend/ui/src/component/auth/SignIn.jsx
+++ frontend/ui/src/component/auth/SignIn.jsx
@@ -4,15 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const SignIn = () => {
   return (
     <div className='flex-col justify-center h-screen w-screen'>
-      <div className='h-1/5 pt-10 pl-10 font-bold text-2xl'>
+      <div className='h-1/5 pt-10 pl-10 font-bold text-2xl w-full'>
       <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> Sign in your account
       </div>
       <div className='flex-col justify-center h-3/5 ml-8 mr-8 '>
-        <div className='h-2/5'> Div1 </div>
-        <div> <input type='text' name='Email'></input> </div>
-        <div>  </div>
-        <div> Div4 </div>
-        <div> Div5 </div>
+        <div className='h-2/5 m-3 '> <img src="/frontend/ui/images/login-icon" alt="Not loaded successfully"></img> </div>
+        <div className='border-b-2 border-gray-300 p-3 m-3' > <input type='text' name='Email' className='w-full font-bold' placeholder='Name'></input> </div>
+        <div className='border-b-2 border-gray-300 p-3 m-3'> <input type='password' name='Email' className='w-full font-bold' placeholder='Password'></input> </div>
+        <div className='flex items-center justify-center m-10 text-white bg-blue-500 p-3 rounded-xl text-xl font-bold'> Sign In </div>
+        <div className='flex items-center justify-center ml-10 mr-10 p-3 '> Don't have an account? <span className='text-blue-500 pl-2 '> Sign Up </span> </div>
       </div>
     </div>
   )

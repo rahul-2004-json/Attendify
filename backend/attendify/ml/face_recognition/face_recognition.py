32034bb0b5a471f7b600ab0f2347b45801c827e0
Anand Panda <anand612p@gmail.com>
2024-10-31 03:24:37 +0530

    rebuild add student api, modularize code, make utils, implement encoding generating functions in face_recognition.py

---
+++ backend/attendify/ml/face_recognition/face_recognition.py
@@ -0,0 +1,66 @@
+import face_recognition
+import cloudinary
+import cloudinary.api
+import numpy as np
+from utils.loadImageFromUrl import load_image_from_url
+
+def get_encodings(np_image):
+    """
+    Function to generate face encodings for a given image.
+    
+    Parameters:
+    np_image (numpy.ndarray): The image in NumPy array format.
+    """
+    # Generate face encodings directly
+    face_encodings = face_recognition.face_encodings(np_image)
+    
+    # If no faces are found, return None
+    if not face_encodings:
+        return None
+    
+    print("Encodings found");
+
+    return face_encodings[0]  # Return the first (and assumed only) face encoding
+
+
+def generate_all_encodings(enroll, cloudinary_folder_path):
+    """
+    Function to generate face encodings for new students and update their records in the database.
+    """
+    try:
+        # Fetch images from Cloudinary folder
+        resources = cloudinary.api.resources_by_asset_folder(cloudinary_folder_path, max_results=15)  # Adjust max_results as needed
+        image_urls_in_folder = []
+
+        for resource in resources['resources']:
+            image_url = resource['secure_url']
+            image_urls_in_folder.append(image_url)
+
+        # TODO : If only 1 image, augment the image and then generate encodings 
+
+        # Initialize a list to hold encodings
+        encodings = []
+
+        # Process each image to generate face encodings
+        for image_url in image_urls_in_folder:
+            # Load the image from cloudinary
+            np_image = load_image_from_url(image_url)
+            # Generate face encodings
+            face_encodings = get_encodings(np_image)
+
+            # Check if encodings were found
+            if face_encodings is not None:
+                encodings.append(face_encodings)
+
+        # If no encodings were found, handle the case (e.g., log an error or skip updating)
+        if not encodings:
+            print(f"No face encodings found for student {enroll}.")
+            return None
+
+        # Convert encodings list to a numpy array
+        encodings_array = np.array(encodings).tolist()  # Convert to list for MongoDB compatibility
+
+        return encodings_array
+
+    except Exception as e:
+        print(f"Error processing student {enroll}: {str(e)}")

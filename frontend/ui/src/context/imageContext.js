200865c7424682b5cb0304eca5dca3c3a9ec8c6e
rahul_004 <rahulbky2004@gmail.com>
2024-11-13 00:25:25 +0530

    Fetch from Ansh

--- frontend/ui/src/context/imageContext.js
+++ frontend/ui/src/context/imageContext.js
@@ -1,31 +1,13 @@
 import React, { createContext, useState } from "react";
 
-export const ImageContext = createContext(null);
+export const ImageContext = createContext();
 
-export default function ImageState({ children }) {
+export const ImageProvider = ({ children }) => {
   const [imagesArray, setImagesArray] = useState([]);
-  const [forwardImages, setforwardImages] = useState([]);
-  const [imageLinks, setImageLinks] = useState([]);
-  const [detectedFaces, setDetectedFaces] = useState([]); //for each image
-  const [isLoading, setIsLoading] = useState(false);
-  // console.log("Forward Images Array:", forwardImages);
-  // console.log("Detected Faces from context:", detectedFaces);
+
   return (
-    <ImageContext.Provider
-      value={{
-        imagesArray,
-        setImagesArray,
-        isLoading,
-        setIsLoading,
-        detectedFaces,
-        setDetectedFaces,
-        setforwardImages,
-        forwardImages,
-        imageLinks,
-        setImageLinks,
-      }}
-    >
+    <ImageContext.Provider value={{ imagesArray, setImagesArray }}>
       {children}
     </ImageContext.Provider>
   );
-}
+};
